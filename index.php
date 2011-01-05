07417e3ebebadf17403b61804f2447aec979b3b4
Brandon R. Stoner <monokrome@monokro.me>
2011-01-04 17:30:26 -0800

    Modified file search to not include processed image files.

--- index.php
+++ index.php
@@ -13,12 +13,31 @@
 
 		foreach ($format_list as $format)
 		{
+				// Make sure that this isn't an already processed file
+			$processed_extension = 'processed.' . $format;
+			$extension_length = strlen($processed_extension);
 			$matched_filenames = glob('*.' . $format);
 
 				// Loop through each file and process it
 			if (count($matched_filenames) > 0)
+			{
 				foreach ($matched_filenames as $filename)
+				{
+
+					$extension_matched = substr_compare(
+						 $filename,
+						 $processed_extension,
+						-$extension_length,
+						 $extension_length
+					) === 0;
+
+					if ($extension_matched)
+						break;
+
 					process_image($filename, $type);
+				}
+			}
+
 		}
 	}
 
